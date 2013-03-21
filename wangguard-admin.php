64165c59bac1ef3fca5897c474c5653afa6a3d18
José Conti <j.conti@joseconti.com>
2013-03-21 18:50:42 +0100

    Tagging WangGuard 1.5.4

--- wangguard-admin.php
+++ wangguard-admin.php
@@ -3,7 +3,7 @@
 Plugin Name: WangGuard
 Plugin URI: http://www.wangguard.com
 Description: <strong>Stop Sploggers</strong>. It is very important to use <a href="http://www.wangguard.com" target="_new">WangGuard</a> at least for a week, reporting your site's unwanted users as sploggers from the Users panel. WangGuard will learn at that time to protect your site from sploggers in a much more effective way. WangGuard protects each web site in a personalized way using information provided by Administrators who report sploggers world-wide, that's why it's very important that you report your sploggers to WangGuard. The longer you use WangGuard, the more effective it will become.
-Version: 1.5.3
+Version: 1.5.4
 Author: WangGuard
 Author URI: http://www.wangguard.com
 License: GPL2
@@ -25,7 +25,7 @@ License: GPL2
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define('WANGGUARD_VERSION', '1.5.3');
+define('WANGGUARD_VERSION', '1.5.4');
 define('WANGGUARD_PLUGIN_FILE', 'wangguard/wangguard-admin.php');
 define('WANGGUARD_README_URL', 'http://plugins.trac.wordpress.org/browser/wangguard/trunk/readme.txt?format=txt');
 
@@ -609,7 +609,7 @@ function wangguard_register_add_question(){
 			
 			$html = '
 					<p>
-							<label for="login_username">' . $question . '</label>
+							<label>' . $question . '</label>
 							<input tabindex="3" type="text" name="wangguardquestansw" id="wangguardquestansw" value="" class="required">
 							<input type="hidden" name="wangguardquest" value="'.$questionID.'" />
 					</p>
