e606af78952dd08acd37573adc2dcf9f8f385818
José Conti <j.conti@joseconti.com>
2013-07-02 15:20:08 +0200

    Migrating to Array and adding new data to User info page
    Now, I'm adding all data. The CSS, DIV, etc.. will be needed. This is
    the first pass for only get all data.

--- wangguard-user-info.php
+++ wangguard-user-info.php
@@ -2,12 +2,29 @@
 //WangGuard Users Info
 
 function wangguard_users_info() {
-	global $wpdb,$wangguard_nonce, $wangguard_api_key,$wangguard_api_host , $wangguard_rest_path;
+	global $wpdb,$wangguard_nonce, $wangguard_api_key,$wangguard_api_host , $wangguard_rest_path, $blog_id;
 	
 
 	if ( !current_user_can('level_10') )
 		die(__('Cheatin&#8217; uh?', 'wangguard'));
 
-		echo "User ID:" . $_GET["userID"] . "<br>";
-	}
+		$userID = $_GET["userID"];
+		$userIP = $_GET["userIP"];
+		$user_info = get_userdata($userID);
+		
+	$blogID = $user_info->primary_blog;
+	$blog_details = get_blog_details( array( 'blog_id' => $blogID ) );
+
+      echo 'Username: ' . $user_info->user_login . '<br />';
+      echo 'User ID: ' . $user_info->ID . '<br />';
+      echo 'User IP: ' . $userIP . '<br />';
+      echo 'User nicename: ' . $user_info->user_nicename . '<br />';
+      echo 'User email: ' . $user_info->user_email . '<br />';
+      echo 'User registered: ' . $user_info->user_registered . '<br />';
+      echo 'User nickname: ' . $user_info->nickname . "<br />";
+      echo 'Blog ID: ' . $blogID . '<br />';
+      echo 'User primary Blog : ' . $blog_details->blogname . '<br />';
+
+}
+      
 ?>
